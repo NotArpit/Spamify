9894a85e3aa7af17de84e1a27da57c9230d1f7f6
Arpit Sharma <43474262+NotArpit@users.noreply.github.com>
2019-02-20 12:38:25 +1100

    Add files via upload
    Completed SVM Project.

---
+++ dataset3Params.m
@@ -0,0 +1,51 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+values_length = length(values);
+
+% Compute the 'base' error using default values of C = 1, sigma = 0.3 :
+model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+predictions = svmPredict(model, Xval);
+error_min = mean(double(predictions ~= yval));
+
+% Nested for loop to consider every combination of [C, sigma] values:
+for C_index = 1:values_length,
+	for sigma_index = 1:values_length,
+		model = svmTrain(X, y, values(C_index), @(x1, x2)gaussianKernel(x1, x2, values(sigma_index)));
+		predictions = svmPredict(model, Xval);
+		curr_error = mean(double(predictions ~= yval));
+		if curr_error < error_min,
+			error_min = curr_error;
+			C = values(C_index);
+			sigma = values(sigma_index);
+		end
+	end
+end
+
+
+% =========================================================================
+
+end
